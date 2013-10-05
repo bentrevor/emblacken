57d08509d8d7812d0caa8b7133020050fa039878
Ben Trevor <benjamin.trevor@gmail.com>
2013-10-05 13:33:34 -0500

    Prettify, make first execution only change text/bg color

--- emblacken.js
+++ emblacken.js
@@ -1,13 +1,24 @@
+if (typeof emblackenCallCount == 'undefined') {
+  emblackenCallCount = 0;
+}
+
 function removeThirdPartyElements() {
-  function R(w) {
+
+  removeElementsFrom(self);
+  removeElementsFromAllFrames();
+
+  function removeElementsFrom(thisWindow) {
     try {
-      var d = w.document, j, i, tagName, T, oldDiv, newDiv, r = 1, C;
+      var d = thisWindow.document;
+      var tagNames = ["object","embed","applet","iframe"];
+      var exitStatus = 1;
+      var j, i, tagName, elements, oldDiv, newDiv, contentWindow;
 
-      for(j = 0; tagName = ["object","embed","applet","iframe"][j]; j++) {
-        T = d.getElementsByTagName(tagName);
+      for(j = 0; tagName = tagNames[j]; j++) {
+        elements = d.getElementsByTagName(tagName);
 
-        for(i = T.length - 1; (i + 1) && (oldDiv = T[i]); --i) {
-          if(j != 3 || !R((C = oldDiv.contentWindow) ? C : oldDiv.contentDocument.defaultView)) {
+        for(i = elements.length - 1; (i + 1) && (oldDiv = elements[i]); --i) {
+          if(moreElementsToRemove(tagName, contentWindow, oldDiv)) {
             newDiv = d.createElement("div");
             newDiv.style.width = oldDiv.width;
             newDiv.style.height = oldDiv.height;
@@ -18,15 +29,30 @@ function removeThirdPartyElements() {
         }
       }
     } catch(E) {
-      r = 0;
+      exitStatus = 0;
+      console.log("something went wrong: " + E);
     }
-    return r;
+
+    return exitStatus;
   }
 
-  R(self);
-  var i, x;
-  for(i = 0; x = frames[i]; i++) {
-    R(x)
+  function moreElementsToRemove(tagName, contentWindow, oldDiv) {
+    return notIframe(tagName) || noMoreThirdPartyElements(contentWindow, oldDiv);
+  }
+
+  function noMoreThirdPartyElements(contentWindow, oldDiv) {
+    return !removeElementsFrom((contentWindow = oldDiv.contentWindow) ? contentWindow : oldDiv.contentDocument.defaultView);
+  }
+
+  function notIframe(tagName) {
+    return tagName != "iframe";
+  }
+
+  function removeElementsFromAllFrames() {
+    var i, frame;
+    for(i = 0; frame = frames[i]; i++) {
+      removeElementsFrom(frame)
+    }
   }
 } 
 
@@ -72,13 +98,20 @@ function stringStylesheet() {
     "}";
 }
 
-javascript:(function(){var d=document; function K(N,w) { var nn = d.createElement(w), C = N.childNodes, i; for(i=C.length-1;i>=0;--i) nn.insertBefore(C[i],nn.childNodes[0]); N.parentNode.replaceChild(nn,N); } function Z(t,w) { var T = document.getElementsByTagName(t), j; for (j=T.length-1;j>=0;--j) K(T[j],w); } Z("blink", "span"); Z("marquee", "div"); })(); 
-
-javascript:(function(){var H=["mouseover","mouseout","unload","resize"],o=window.opera; if(document.addEventListener/*MOZ*/&&!o) for(j in H)document.addEventListener(H[j],function(e){e.stopPropagation();},true); else if(window.captureEvents/*NS4*/&&!o) { document.captureEvents(-1/*ALL*/);for(j in H)window["on"+H[j]]=null;} else/*IE*/ {function R(N){var i,x;for(j in H)if(N["on"+H[j]]/*NOT TEXTNODE*/)N["on"+H[j]]=null;for(i=0;x=N.childNodes[i];++i)R(x);}R(document);}})(); 
-
-javascript:(function() { var c, tID, iID; tID = setTimeout(function(){}, 0); for (c=1; c<1000 && c<=tID; ++c) clearTimeout(tID - c); iID = setInterval(function(){},1000); for (c=0; c<1000 && c<=iID; ++c) clearInterval(iID - c); })();
-
-removeThirdPartyElements();
 useBlackTextOnWhiteBackground();
 
-console.log("got to the end");
+if (emblackenCallCount > 0) {
+
+  removeThirdPartyElements();
+
+
+  javascript:(function(){var d=document; function K(N,w) { var nn = d.createElement(w), C = N.childNodes, i; for(i=C.length-1;i>=0;--i) nn.insertBefore(C[i],nn.childNodes[0]); N.parentNode.replaceChild(nn,N); } function Z(t,w) { var T = document.getElementsByTagName(t), j; for (j=T.length-1;j>=0;--j) K(T[j],w); } Z("blink", "span"); Z("marquee", "div"); })(); 
+
+  javascript:(function(){var H=["mouseover","mouseout","unload","resize"],o=window.opera; if(document.addEventListener/*MOZ*/&&!o) for(j in H)document.addEventListener(H[j],function(e){e.stopPropagation();},true); else if(window.captureEvents/*NS4*/&&!o) { document.captureEvents(-1/*ALL*/);for(j in H)window["on"+H[j]]=null;} else/*IE*/ {function R(N){var i,x;for(j in H)if(N["on"+H[j]]/*NOT TEXTNODE*/)N["on"+H[j]]=null;for(i=0;x=N.childNodes[i];++i)R(x);}R(document);}})(); 
+
+  javascript:(function() { var c, tID, iID; tID = setTimeout(function(){}, 0); for (c=1; c<1000 && c<=tID; ++c) clearTimeout(tID - c); iID = setInterval(function(){},1000); for (c=0; c<1000 && c<=iID; ++c) clearInterval(iID - c); })();
+}
+
+
+
+console.log("finished emblackening " + emblackenCallCount++ + " times.");
